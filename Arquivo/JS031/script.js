ef0b1f61ee6bd31c790ec1b18d39740abb6673a3
ViniFerAlbuquerque <viniferalbuquerque@gmail.com>
2025-11-05 21:21:04 -0300

    JoKenPo

---
+++ Arquivo/JS031/script.js
@@ -0,0 +1,35 @@
+function jogar() {
+    const jogadorHumano = document.querySelector('input[name="jogador"]:checked')
+
+    if (!jogadorHumano) {
+        alert('[[ERRO!] ► Escolha sua jogada!]')
+        return 
+    } 
+    const jogadas = ['Pedra', 'Papel', 'Tesoura'] 
+    const jogadorIA = jogadas[Math.floor(Math.random() * jogadas.length)] 
+    const escolhaHumano = jogadorHumano.value // definir value="Pedra", etc., no HTML
+
+    let resultado = ''
+     
+    if (escolhaHumano === jogadorIA) {
+        resultado = 'Empate! Tente de novo!'
+    } else if (
+        (escolhaHumano === 'Pedra' && jogadorIA === 'Tesoura') ||
+        (escolhaHumano === 'Tesoura' && jogadorIA === 'Papel') || 
+        (escolhaHumano === 'Papel' && jogadorIA === 'Pedra')
+        ) {
+            resultado = 'Você GANHOU!'
+        } else {
+            resultado = 'Você PERDEU!'
+        } resposta.innerHTML=`<p>${resultado}</p>
+        <p>A IA jogou <strong>${jogadorIA}</strong></p>`  
+}
+const radios = document.querySelectorAll('input[name="jogador"]') 
+const resposta = document.querySelector('article.resposta') 
+
+radios.forEach(radio => {
+    radio.addEventListener('change',() => {
+        resposta.innerHTML= ''
+    })
+})
+
