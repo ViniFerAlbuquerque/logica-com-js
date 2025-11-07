7f364c0445f9230bb483dff364247b63f2b6013e
ViniFerAlbuquerque <viniferalbuquerque@gmail.com>
2025-11-07 18:34:59 -0300

    Pontos de Saúde

---
+++ Arquivo/JS036/script.js
@@ -0,0 +1,27 @@
+const inputHoras = document.getElementById('horas')
+const resposta = document.querySelector('div.resposta')
+
+function calcular() {
+    let horas = Number(inputHoras.value)
+
+    if (isNaN(horas) || horas <= 0) {
+        alert('[ERRO]→Insira valores válidos!')
+        return
+    }
+
+    let pontos = 0
+
+    if (horas <= 10) {
+        pontos = horas * 2
+    } else if (horas <= 20) {
+        pontos = horas * 5
+    } else {
+        pontos = horas * 10
+    }
+
+    const valorRecebido = pontos * 0.05
+
+    resposta.innerText = `Total de pontos: ${pontos}
+ Valor: ${valorRecebido.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`
+     resposta.style.color='#FF0'
+}
