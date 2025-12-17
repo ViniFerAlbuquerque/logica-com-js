695ac744d8468a25c9a522fe05371a7173aa0c52
ViniFerAlbuquerque <viniferalbuquerque@gmail.com>
2025-12-17 10:23:51 -0300

    Sorteador de 20 números correções

--- Arquivo/JS050/script.js
+++ Arquivo/JS050/script.js
@@ -1,36 +1,12 @@
-const resposta = document.querySelector('div.resposta')
+// 
 
-
-
-// document.getElementById('button').onclick = sortear()
-
-function sortear() {
-    alert("Função")
-
-
-
-    let contador = 0
-
-    let exibicao = []
-
-    let maior5 = 0
-    let divisivel3 = 0
-
-    while (contador < 20) {
-        let numeros = Math.floor(Math.random() * 11)
-        exibicao.push(numeros)
-        if (numeros > 5) {
-            maior5++
-        } else if (numeros % 3 == 0 && numeros != 0) {
-            divisivel3++
-        }
-        contador++
-    }
-    // const resposta = document.querySelector('div.resposta')
-
-    resposta.innerHTML = <p>`Números sorteados: ${exibicao.join(' , ')}`</p>
-
-    resposta.innerHTML += `<p>Maiores que 5: ${maior5} números
-                            Divisível por 3: ${divisivel3} números</p>`
-}
-
+function sortear() { 
+    const numeros = []
+    for (let i = 0; i < 20; i++) {
+        const n = Math.floor(Math.random() * 11)
+        numeros.push(n)
+        
+        const alvo = document.getElementById('resposta') || document.querySelector('.resposta')
+        if (alvo) { alvo.textContent = numeros.join(', ')}
+     }
+ }
